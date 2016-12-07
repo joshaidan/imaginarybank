2d42640fd9d18482d3e7d1fb94c32b2305487f38
Brian Jones <joshaidan@gmail.com>
2016-12-07 16:23:57 -0500

    Add session tests

---
+++ test/integration/sessions_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class SessionsTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'user required to login' do
+    visit accounts_url
+    assert_equal new_user_session_path, current_path
+  end
+
+  test 'user can sign up' do
+    visit new_user_session_path
+    assert_equal new_user_session_path, current_path
+    click_link 'Sign up'
+    assert_equal new_user_registration_path, current_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    assert_equal root_path, current_path
+  end
+end
